659da8822fcd67e5cc763ca270b86d4982bf8fe9
Your Name <you@example.com>
2020-12-31 23:58:43 -0700

    project complete

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('category', 'CategoryController')->middleware('auth');
 Route::resource('food', 'FoodController')->middleware('auth');
 
-Route::get('/','FoodController@listFood');
+Route::get('/', 'FoodController@listFood');
 
-Route::get('/foods/{id}','FoodController@view')->name('food.view');
+Route::get('/foods/{id}', 'FoodController@view')->name('food.view');
